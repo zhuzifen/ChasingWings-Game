8c6f07b6c81672d97baa40a23981430fc4da1bc5
erinzhang1 <qq596819255@gmail.com>
2021-11-03 14:52:12 -0400

    add door to environment control

--- Assets/script/EnvironmentControl.cs
+++ Assets/script/EnvironmentControl.cs
@@ -6,6 +6,7 @@ public class EnvironmentControl : MonoBehaviour
 {
     public GameObject key;
     public GameObject memoryPiece;
+    public GameObject door;
 
     private Vector3 keyPosition = new Vector3(0, -2, 24);
     private Vector3 keyRoation = new Vector3(0, 50, 0);
@@ -13,13 +14,18 @@ public class EnvironmentControl : MonoBehaviour
     private Vector3 memoryPiecePosition = new Vector3(0, 11, 21);
     private Vector3 memoryPieceRotation = new Vector3(37, 0, 0);
 
+    private Vector3 doorPosition = new Vector3(0, 3.9f, 49);
+    private Vector3 doorRotation = new Vector3(0, 90, 0);
+
     private GameObject keyNow;
     private GameObject memoryPieceNow;
+    private GameObject doorNow;
     // Start is called before the first frame update
     void Start()
     {
         keyNow = GameObject.Instantiate(key, keyPosition, Quaternion.Euler(keyRoation));
         memoryPieceNow = GameObject.Instantiate(memoryPiece, memoryPiecePosition, Quaternion.Euler(memoryPieceRotation));
+        doorNow = GameObject.Instantiate(door, doorPosition, Quaternion.Euler(doorRotation));
     }
 
     // Update is called once per frame
@@ -37,6 +43,10 @@ public class EnvironmentControl : MonoBehaviour
         if (!memoryPieceNow)
         {
             memoryPieceNow = GameObject.Instantiate(memoryPiece, memoryPiecePosition, Quaternion.Euler(memoryPieceRotation));
+        }
+        if (!doorNow)
+        {
+            doorNow = GameObject.Instantiate(door, doorPosition, Quaternion.Euler(doorRotation));
         }
     }
 }

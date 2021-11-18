aa97b70e0893112dc90796e91465d63aa9a0331d
wang_w571 <wang_w571@hotmail.com>
2021-11-19 00:52:26 +0800

    Directional Board Demo

---
+++ Assets/script/Level Items Script/BaseDirectionalBoard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace script.Level_Items_Script
+{
+    public class BaseDirectionalBoard : BaseLevelItemScript
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            characterMove cm;
+            charSimulate cs;
+            if (other.TryGetComponent(out cm))
+            {
+                cm.move *= -1;
+                cm.transform.eulerAngles += Vector3.up * 180;
+                Destroy(this);
+            }
+            if (other.TryGetComponent(out cs))
+            {
+                cs.move *= -1;
+                cm.transform.eulerAngles += Vector3.up * 180;
+                // cm.rb.AddForce(this.transform.up * ForceMultiplier, ForceMode.Impulse);
+                Destroy(this);
+            }
+        }
+    }
+}

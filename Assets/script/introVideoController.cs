cbd3036a148904a181325fae156c0c88981d2113
erinzhang1 <qq596819255@gmail.com>
2021-11-24 18:02:40 -0500

    add level selection

--- Assets/script/introVideoController.cs
+++ Assets/script/introVideoController.cs
@@ -11,6 +11,9 @@ public class introVideoController : MonoBehaviour
     public VideoPlayer vpLoop;
     public GameObject start;
     public GameObject exit;
+    public GameObject tutLevel;
+    public GameObject level1;
+    public GameObject level2;
 
     // Update is called once per frame
     void Update()
@@ -20,6 +23,9 @@ public class introVideoController : MonoBehaviour
             img.texture = vpLoop.texture;
             start.SetActive(true);
             exit.SetActive(true);
+            tutLevel.SetActive(true);
+            level1.SetActive(true);
+            level2.SetActive(true);
         } else
         {
             img.texture = vpStart.texture;

71efb9c890708dbe06a2ec07d46eebe1fc313daa
erinzhang1 <qq596819255@gmail.com>
2021-11-24 00:51:17 -0500

    second level and small changes for first two

--- Assets/script/EnvironmentControl.cs
+++ Assets/script/EnvironmentControl.cs
@@ -5,27 +5,39 @@ using UnityEngine;
 public class EnvironmentControl : MonoBehaviour
 {
     public GameObject key;
-    public GameObject memoryPiece;
+    public GameObject bonus1;
+    public GameObject bonus2;
     public GameObject door;
 
     public Vector3 keyPosition = new Vector3(0, -0.8f, 9.5f);
     public Vector3 keyRoation = new Vector3(0, 50, 0);
 
-    public Vector3 memoryPiecePosition = new Vector3(0, 3.8f, 10);
-    public Vector3 memoryPieceRotation = new Vector3(37, 0, 0);
+    public Vector3 bonus1Position = new Vector3(0, 3.8f, 10);
+    public Vector3 bonus1Rotation = new Vector3(37, 0, 0);
+
+    public Vector3 bonus2Position = new Vector3(0, 3.8f, 10);
+    public Vector3 bonus2Rotation = new Vector3(37, 0, 0);
 
     public Vector3 doorPosition = new Vector3(0, 1.55f, 20);
     public Vector3 doorRotation = new Vector3(0, 90, 0);
 
     private GameObject keyNow;
-    private GameObject memoryPieceNow;
+    private GameObject bonus1Now;
+    private GameObject bonus2Now;
     private GameObject doorNow;
     // Start is called before the first frame update
     void Start()
     {
         keyNow = GameObject.Instantiate(key, keyPosition, Quaternion.Euler(keyRoation));
-        memoryPieceNow = GameObject.Instantiate(memoryPiece, memoryPiecePosition, Quaternion.Euler(memoryPieceRotation));
         doorNow = GameObject.Instantiate(door, doorPosition, Quaternion.Euler(doorRotation));
+        if (bonus1)
+        {
+            bonus1Now = GameObject.Instantiate(bonus1, bonus1Position, Quaternion.Euler(bonus1Rotation));
+        }
+        if (bonus2)
+        {
+            bonus1Now = GameObject.Instantiate(bonus2, bonus2Position, Quaternion.Euler(bonus2Rotation));
+        }
     }
 
     // Update is called once per frame
@@ -40,9 +52,13 @@ public class EnvironmentControl : MonoBehaviour
         {
             keyNow = GameObject.Instantiate(key, keyPosition, Quaternion.Euler(keyRoation));
         }
-        if (!memoryPieceNow)
+        if (!bonus1Now)
         {
-            memoryPieceNow = GameObject.Instantiate(memoryPiece, memoryPiecePosition, Quaternion.Euler(memoryPieceRotation));
+            bonus1Now = GameObject.Instantiate(bonus1, bonus1Position, Quaternion.Euler(bonus1Rotation));
+        }
+        if (!bonus2Now)
+        {
+            bonus2Now = GameObject.Instantiate(bonus2, bonus2Position, Quaternion.Euler(bonus2Rotation));
         }
         if (!doorNow)
         {
--- Assets/script/SceneSwitch.cs
+++ Assets/script/SceneSwitch.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
-    public void click()
+    public void SwitchScene1()
     {
         SceneManager.LoadScene(1);
+    }
+    public void SwitchScene2()
+    {
+        SceneManager.LoadScene(2);
     }
 }

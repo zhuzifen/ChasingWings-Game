85a05693226016a39f09c791367b4ab1ae6d7a7f
wang_w571 <wang_w571@hotmail.com>
2021-11-29 00:28:59 +0800

    Task for spencer

--- Assets/script/LevelUIScript.cs
+++ Assets/script/LevelUIScript.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace script
 {
@@ -7,8 +9,33 @@ namespace script
     {
         public List<Sprite> CompletionSpriteLoops;
         
+        public List<Sprite> EnterCompleteSpriteLoops;
+        
         public List<Sprite> InProgressSpriteLoops;
+
+        private bool Collected;
+
+        public Image DisplayImage;
+
+        private void FixedUpdate()
+        {
+            GoThroughSprites();
+        }
         
-        
+        // TODO:
+        // When Collected is false, go through and loop the InProgressSpriteLoops in 60 fps and make sure it follows the actual game time
+        // When it's true, go through EnterCompleteSpriteLoops once first and then loop with CompletionSpriteLoops
+        // The target is DisplayImage.sprite
+        // You can make any fields and methods as you want
+        void GoThroughSprites()
+        {
+
+            
+        }
+
+        public void MarkCollected()
+        {
+            Collected = true;
+        }
     }
 }
